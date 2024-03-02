ce054b0f9cc33f274aa906de7c0dc2c149a3adb8
R2377 <rudragayatri5@gmail.com>
2024-03-02 14:44:21 +0530

    initial commit

---
+++ courses/urls.py
@@ -0,0 +1,8 @@
+# myapp/urls.py
+
+from django.urls import path
+from .views import CourseListCreateView
+
+urlpatterns = [
+    path('courses/', CourseListCreateView.as_view(), name='course-list-create'),
+]
