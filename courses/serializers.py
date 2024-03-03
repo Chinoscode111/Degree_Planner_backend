5268759dfe160d1edcdac723ea624ebeda698d23
R2377 <rudragayatri5@gmail.com>
2024-03-03 11:40:03 +0530

    modifying serializers.py

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,9 +1,23 @@
-# myapp/serializers.py
-
 from rest_framework import serializers
 from .models import Course
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['course_title', 'course_code', 'tag', 'semester', 'credits']
+
+    def validate(self, data):
+        """
+        Validate that semester and credits are greater than 0.
+        """
+        semester = data.get('semester')
+        credits = data.get('credits')
+
+        if semester <= 0:
+            raise serializers.ValidationError("Semester must be greater than 0.")
+
+        if credits <= 0:
+            raise serializers.ValidationError("Credits must be greater than 0.")
+
+        return data
+
